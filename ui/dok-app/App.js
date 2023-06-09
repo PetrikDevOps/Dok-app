3afd4c9c174b19028170973a7d3d74b7d74e3c5d
drinkmonster <berenyibence98@gmail.com>
2023-06-09 09:04:08 +0200

    majd ottttthon megtanulok reagalas js-ezni

--- ui/dok-app/App.js
+++ ui/dok-app/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const dockospfp = require('./assets/favicon.png');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff' }}>Döckös Pali</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#484848',
     alignItems: 'center',
     justifyContent: 'center',
   },
