1a82fe61fff5a74b73bbc8330a5f1aea2bad1603
drinkmonster <berenyibence98@gmail.com>
2023-06-10 09:12:55 +0200

    merge xd

--- ui/dok-app/App.js
+++ ui/dok-app/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
 import profilePage from './pages/profile';
 import loginPage from './pages/login';
-=======
 import btn from './comp/btn.js';
->>>>>>> c3d5352fa091cfb89e117a3fbc70abe228cbf8b3
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
        ̇{loginPage()}
     </div>
-=======
-      {btn()}
-    </div >
->>>>>>> c3d5352fa091cfb89e117a3fbc70abe228cbf8b3
   );
 };
 
