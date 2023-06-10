dfc05dcc7fc6e0331fa5a07aa219ded0dbd0bf54
drinkmonster <berenyibence98@gmail.com>
2023-06-10 08:01:21 +0200

    main

--- ui/dok-app/pages/login.js
+++ ui/dok-app/pages/login.js
@@ -1,22 +1,21 @@
-import { View, StyleSheet, Text, TextInput, TouchableOpacity, Linking } from 'react-native';
-
+import { View, StyleSheet, Text, TouchableOpacity, Linking, Image } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 let loginPage = () => {
-
     const styles = StyleSheet.create({
         container: {
             flex: 1,
             backgroundColor: '#1E1E1E',
             alignItems: 'center',
+            justifyContent: 'flex-start',
             paddingHorizontal: 20,
-            justifyContent: 'center',
-            width: '100vw',
             height: '100vh',
+            width: '100vw',
         },
         logoContainer: {
+            marginTop: '5vh',
             alignItems: 'center',
-            marginBottom: 10,
-            marginTop: '-12vh',
+            marginBottom: '25vh',
         },
         logo: {
             fontSize: 32,
@@ -25,54 +24,26 @@ let loginPage = () => {
         },
         line: {
             width: '100%',
-            marginBottom: '5vh',
             height: 1,
             backgroundColor: '#474747',
             marginVertical: 20,
         },
-        inputContainer: {
-
-            width: '100%',
-            marginTop: 20,
-            marginBottom: 10,
-        },
-        label: {
-            fontSize: 16,
-            color: '#FFFFFF',
-            alignSelf: 'flex-start',
-            marginBottom: 5,
-        },
-        input: {
-            width: '100%',
-            height: 40,
-            backgroundColor: 'rgba(217, 217, 217, 0.32)',
-            marginBottom: 15,
-            paddingHorizontal: 10,
-            borderRadius: 5,
-        },
         loginButton: {
             width: '100%',
-            height: 40,
+            height: 50,
             backgroundColor: '#7BB49F',
             alignItems: 'center',
             justifyContent: 'center',
-            marginTop: '15vh',
-            marginBottom: 15,
+            marginTop: 50,
+            marginBottom: 50,
             borderRadius: 5,
+            flexDirection: 'row',
         },
         loginButtonText: {
             fontSize: 16,
             fontWeight: 'bold',
             color: '#1E1E1E',
-        },
-        signupLink: {
-            marginBottom: 10,
-        },
-        signupLinkText: {
-            fontSize: 14,
-            color: '#7BB49F',
-            textDecorationLine: 'underline',
-            marginBottom: 10,
+            marginLeft: 10,
         },
     });
 
@@ -82,21 +53,15 @@ let loginPage = () => {
                 <Text style={styles.logo}>DÖK App</Text>
             </View>
             <View style={styles.line} />
-            <View style={styles.inputContainer}>
-                <Text style={styles.label}>name</Text>
-                <TextInput style={styles.input} />
-                <Text style={styles.label}>password</Text>
-                <TextInput secureTextEntry={true} style={styles.input} />
-            </View>
             <TouchableOpacity style={styles.loginButton}>
-                <Text style={styles.loginButtonText}>login</Text>
+                <FontAwesome name="microsoft" size={24} color="#1E1E1E" />
+                <Text style={styles.loginButtonText}>Login with Microsoft</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => Linking.openURL('https://lobotomy.store')} style={styles.signupLink}>
-                <Text style={styles.signupLinkText}>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => Linking.openURL('https://lobotomy.store')}>
             </TouchableOpacity>
             <View style={styles.line} />
         </View>
-    )
-}
+    );
+};
 
 export default loginPage;
