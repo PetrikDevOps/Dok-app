15c4e5b4071c3cae6c695ca3c679960d1eb3aa4d
drinkmonster <beni98@cock.li>
2023-06-12 19:16:10 +0200

    main - elkezdtem admin page, vmikor finish

--- ui/sokadik-ui/App.tsx
+++ ui/sokadik-ui/App.tsx
@@ -1,11 +1,12 @@
 import profilePage from './pages/profile';
 import votePage from './pages/vote';
 import loginPage from './pages/login';
+import voteAdminPage from './pages/voteAdmin';
 
 export default function App() {
   return (
     <div>
-      {votePage()}
+      {voteAdminPage()}
     </div>
   );
 }
