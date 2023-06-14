cbc9b825fc8349c0af03ef6d39064046e974dd2b
Susa Milán <mailtosnek@protonmail.com>
2023-06-14 12:44:17 +0200

    Added tasks menu

---
+++ ui/user/lib/pages/profile/tasks.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+
+class Tasks extends StatefulWidget {
+  const Tasks({super.key});
+
+  @override
+  State<Tasks> createState() => _TasksState();
+}
+
+class _TasksState extends State<Tasks> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          CupertinoSliverNavigationBar(
+            largeTitle: Text('Tasks'),
+          ),
+        ],
+        body: CupertinoListSection.insetGrouped(
+          header: Text('Your tasks'),
+          children: [
+            CupertinoListTile.notched(
+              title: Text('Mosd el a tanyerokat XD'),
+              trailing: CupertinoListTileChevron(),
+              leading: Icon(
+                CupertinoIcons.xmark,
+                color: CupertinoColors.systemRed,
+              ),
+            ),
+            CupertinoListTile.notched(
+              title: Text('Fuss 17 kort a keleti palyaudvarnal'),
+              trailing: CupertinoListTileChevron(),
+              leading: Icon(
+                CupertinoIcons.check_mark,
+                color: CupertinoColors.systemGreen,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
