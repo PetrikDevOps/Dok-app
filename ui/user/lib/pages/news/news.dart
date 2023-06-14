3ce67faf0bae28d7a848a00a847dc77f11ea41c3
Susa Milán <mailtosnek@protonmail.com>
2023-06-14 12:06:01 +0200

    Added the NewsCard

--- ui/user/lib/pages/news/news.dart
+++ ui/user/lib/pages/news/news.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:user/pages/news/components/news_card.dart';
 
 class News extends StatelessWidget {
   const News({super.key});
@@ -6,13 +7,22 @@ class News extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        child: NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) => [
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('News'),
+      child: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          CupertinoSliverNavigationBar(
+            largeTitle: Text('News'),
+          ),
+        ],
+        body: Column(
+          children: [
+            NewsCard(
+                title: "Gaming",
+                description: "We do a little gaming!",
+                imageUrl:
+                    'https://images.pexels.com/photos/3171837/pexels-photo-3171837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+          ],
         ),
-      ],
-      body: Text('News'),
-    ));
+      ),
+    );
   }
 }
