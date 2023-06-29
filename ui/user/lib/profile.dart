767347501ef4ed398eced28662558278bc813d1f
Susa Milan <mailtosnek@protonmail.com>
2023-06-29 09:41:29 +0200

    Added getPfp function

--- ui/user/lib/profile.dart
+++ ui/user/lib/profile.dart
@@ -1,3 +1,18 @@
-String getPfp(String cookie) {
-  return 'placeholder';
+import 'package:http/http.dart' as http;
+import 'package:user/glob.dart' as glob;
+
+// Retrieves the profile picture from the backend using the cookie we got from MS
+// TODO: Use the cookie
+Future<String> getPfp(String cookie) async {
+  try {
+    final response = await http.post(Uri.parse('${glob.appUrl}pfp'));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return 'Request failed with status: ${response.statusCode}';
+    }
+  } catch (e) {
+    return 'Errpr: $e';
+  }
 }
